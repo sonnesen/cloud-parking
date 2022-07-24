65a6a410dfc2592affd3501f20f1650af7649f9b
Winston Spencer Sonnesen <sonnesen@gmail.com>
2022-07-24 12:24:02 -0300

    fix(hello-controller): Show hello message for the root URI

--- src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
@@ -1,12 +1,14 @@
 package one.digitalinnovation.cloudparking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class HelloController {
 
-    @GetMapping(value = "/hello")
+    @GetMapping
     public String sayHello() {
         return "Hello DIO.";
     }
